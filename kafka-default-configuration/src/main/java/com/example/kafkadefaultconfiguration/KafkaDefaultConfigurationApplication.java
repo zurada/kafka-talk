46480d6a9ee2b801ccd2d7d538a2ceee0349ca9c
Adam Zurada <adamzurada@gmail.com>
2019-05-06 21:29:31 +0200

    default configuration for Spring Boot + Kafka Streams

---
+++ kafka-default-configuration/src/main/java/com/example/kafkadefaultconfiguration/KafkaDefaultConfigurationApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkadefaultconfiguration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaDefaultConfigurationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaDefaultConfigurationApplication.class, args);
+    }
+
+}
