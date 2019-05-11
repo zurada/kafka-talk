85355670fa379284bb236da89c6c57e12f7bab38
Adam Zurada <adam.zurada1@swisscom.com>
2019-05-11 23:53:00 +0200

    added kafka state listeners

--- README.md
+++ README.md
@@ -1,10 +1,33 @@
 # kafka-talk
 Talk on: How do we use Apache Kafka at Swisscom and what is it for?
 
+## What is it?
+It contains the presentation and code which I used during Swisscom DevOps week for the talk about Kafka.
+The simple examples of setting up Kafka altogether with Spring Boot and Kafka Streams.
+
+
+
 ## Start Kafka
 Run Kafka locally by typing:
 docker-compose -f kafka.yml up
 
-## What is it?
-It contains the presentation and code which I used during Swisscom DevOps week for the talk about Kafka.
-The simple examples of setting up Kafka altogether with Spring Boot and Kafka Streams.
+## Access Kafka
+1. Get container ID by: docker ps
+2. Access image by: docker exec -it <container_id>  bash
+
+## Create topic
+1. Access Kafka
+2. kafka-topics --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic <topic_name>
+
+## List topics
+1. Access Kafka
+2. 
+
+## Produce message
+kafka-console-producer --broker-list localhost:9092  --topic test --property "parse.key=true" --property "key.separator=:"
+
+## Consumer messages
+kafka-console-consumer --bootstrap-server localhost:9092 --topic test --from-beginning
+
+## List topics
+kafka-topics --list --bootstrap-server localhost:9092
--- kafka-default-configuration/src/main/java/com/example/kafkadefaultconfiguration/KafkaConfig.java
+++ kafka-default-configuration/src/main/java/com/example/kafkadefaultconfiguration/KafkaConfig.java
@@ -2,14 +2,22 @@ package com.example.kafkadefaultconfiguration;
 
 import com.example.kafkadefaultconfiguration.model.Dummy;
 import com.example.kafkadefaultconfiguration.serdes.JsonSerde;
+import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.errors.LogAndContinueExceptionHandler;
+import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Produced;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.SmartLifecycle;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,10 +27,16 @@ import java.util.EnumSet;
 import java.util.Objects;
 import java.util.Properties;
 
+import static java.lang.String.format;
+
 @Configuration
 public class KafkaConfig {
 
     private static final Logger logger = LoggerFactory.getLogger(KafkaConfig.class);
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
 
     @Bean
     public KStream<String, Dummy> dummyStream(StreamsBuilder streamsBuilder) {
@@ -62,7 +76,7 @@ public class KafkaConfig {
     }
 
     //SmaltLifecycle coming from Spring
-    public static class KafkaStreamsLifecycle implements SmartLifecycle {
+    public class KafkaStreamsLifecycle implements SmartLifecycle {
 
         private final EnumSet<KafkaStreams.State> RUNNING_STATES =
                 EnumSet.of(KafkaStreams.State.RUNNING, KafkaStreams.State.REBALANCING);
@@ -93,6 +107,8 @@ public class KafkaConfig {
         public void start() {
             if (!isRunning()) {
                 try {
+                    kafkaStreams.setStateListener(getStateListener());
+                    kafkaStreams.setUncaughtExceptionHandler(getUncaughtExceptionHandler());
                     kafkaStreams.start();
                 } catch (Exception e) {
                     throw new RuntimeException("Failed to start kafka streams", e);
@@ -106,4 +122,37 @@ public class KafkaConfig {
         }
 
     }
+
+    /**
+     * @return Default UncaughtExceptionHandler, where we try to close the Application on UncaughtException
+     */
+    private  Thread.UncaughtExceptionHandler getUncaughtExceptionHandler() {
+        return (t, e) -> {
+            logger.info("Uncaught exception in KafkaStream: [{}]. Shutting down Spring application.", e);
+            try {
+            	System.exit(SpringApplication.exit(applicationContext, () -> 0));
+			} catch(IllegalStateException ex) {
+                // ignore - VM is already shutting down
+            }
+        };
+    }
+
+    /**
+     * @return Default KafkaStreams.StateListener, where we try to close the Application on newState == NOT_RUNNING as
+     * a last resort
+     */
+    private KafkaStreams.StateListener getStateListener() {
+        return (newState, oldState) -> {
+            logger.info("Detected KafkaStream state change: [{}] -> [{}]", oldState.name(), newState.name());
+            if(KafkaStreams.State.NOT_RUNNING == newState) {
+                logger.info("KafkaStream is moving into an unwanted state: [{}]. Shutting down Spring application.", newState);
+                try {
+                    System.exit(SpringApplication.exit(applicationContext, () -> 0));
+                } catch(IllegalStateException ex) {
+                    // ignore - VM is already shutting down
+                }
+            }
+        };
+    }
+
 }
